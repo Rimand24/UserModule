89fb5783b83c410a324c5a01bf0e4ce31885b038
Rimand <Spiculum24@gmail.com>
2020-06-22 22:42:39 +0300

    html

--- src/main/java/org/example/auth/service/registration/RegistrationRequest.java
+++ src/main/java/org/example/auth/service/registration/RegistrationRequest.java
@@ -3,18 +3,15 @@ package org.example.auth.service.registration;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.example.auth.domain.Role;
 
-import javax.persistence.*;
 import java.io.Serializable;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistrationRequest implements Serializable {
 
-    private static final long serialVersionUID = 2331825251467749095L;
+    private static final long serialVersionUID = -1064645942032271857L;
     private String username;
     private String password;
     private String email;
