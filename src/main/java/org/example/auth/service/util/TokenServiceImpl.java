4b3f78b75b3dccd4b46adde344efe37d392e459c
Rimand <Spiculum24@gmail.com>
2020-07-09 00:49:58 +0300

    token service refactoring

--- src/main/java/org/example/auth/service/util/TokenServiceImpl.java
+++ src/main/java/org/example/auth/service/util/TokenServiceImpl.java
@@ -1,7 +1,6 @@
 package org.example.auth.service.util;
 
 import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +9,14 @@ import java.util.Date;
 @Service
 public class TokenServiceImpl implements TokenService {
 
-    public final long authExpirationTime;
-    public final long activationCodeExpirationTime;
-    public final long passwordResetExpirationTime;
-    public final String tokenSecret;
-
-    @Autowired
-    public TokenServiceImpl(@Value("${security.expiration-time.auth}") long authExpirationTime,
-                            @Value("${security.expiration-time.activation-code}") long activationCodeExpirationTime,
-                            @Value("${security.expiration-time.password-reset}") long passwordResetExpirationTime,
-                            @Value("${security.tokenSecret}") String tokenSecret) {
-        this.authExpirationTime = authExpirationTime;
-        this.activationCodeExpirationTime = activationCodeExpirationTime;
-        this.passwordResetExpirationTime = passwordResetExpirationTime;
-        this.tokenSecret = tokenSecret;
-    }
+    @Value("${security.expiration-time.auth:86400000}")
+    public long authExpirationTime;
+    @Value("${security.expiration-time.activation-code:86400000}")
+    public long activationCodeExpirationTime;
+    @Value("${security.expiration-time.password-reset:86400000}")
+    public long passwordResetExpirationTime;
+    @Value("${security.tokenSecret}")
+    public String tokenSecret;
 
     @Override
     public String generateEmailVerificationToken() {
