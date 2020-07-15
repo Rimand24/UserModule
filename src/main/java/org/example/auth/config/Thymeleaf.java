59489d08beccb693e691a02bce68b5591efcc79b
Rimand <Spiculum24@gmail.com>
2020-07-15 22:58:41 +0300

    +thymeleaf layout

---
+++ src/main/java/org/example/auth/config/Thymeleaf.java
@@ -0,0 +1,17 @@
+package org.example.auth.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Thymeleaf {
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+}
+
+
